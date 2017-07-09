7125cbf2f861d1ba21ad1ad6e5d7f30c1fd843de
David Espinoza <despinoza1027@gmail.com>
2017-07-08 21:34:07 -0500

    initial commit

---
+++ imports/ui/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <div className="container">
+        <p>hi homie</p>
+      </div>
+    );
+  }
+}
