9a045d7ebb942b31b37d18b730312eec291dc8d3
David Espinoza <despinoza1027@gmail.com>
2017-07-08 22:45:32 -0500

    game mech framework set up

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
+import GameMechanics from './GameMechanics.jsx';
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      playerDecision: '',
+      computerDecision: '',
+    };
+  }
+
+  playerSelect(evt) {
+    this.setState({
+      playerDecision: evt.target.value,
+    })
+  }
+
+  playGame() {
+    let options = ["rock", "paper", "scissors", "spock", "lizard"];
+    let computerSelection = options[Math.floor(Math.random() * 5)]
+    GameMechanics.rps(this.state.playerDecision, computerSelection);
+  };
 
   render() {
     return (
       <div className="container">
-        <p>hi homie</p>
+        <p>Rock-Paper-Scissors-Spock-Lizard</p>
+        <select onChange={ (evt) => this.playerSelect(evt) }>
+          <option value="rock">Rock</option>
+          <option value="paper">Paper</option>
+          <option value="scissors">Scissors</option>
+          <option value="spock">Spock</option>
+          <option value="lizard">Lizard</option>
+        </select>
+        <button onClick={ this.playGame() }>Play</button>
       </div>
     );
   }
---
+++ imports/ui/GameMechanics.jsx
@@ -0,0 +1,79 @@
+const GameMechanics = {
+
+  rps(me, computer) {
+    let result;
+    // ROCK RESULTS
+    if (me === 'rock' && computer === 'scissors') {
+      result = 'Rock crushes scissors - You win';
+    }
+    else if (me === 'rock' && computer === 'lizard') {
+      result = 'Rock crushes lizard - You win';
+    }
+    else if (me === 'rock' && computer === 'paper') {
+      result = 'Rock is covered by paper - You lose';
+    }
+    else if (me === 'rock' && computer === 'spock') {
+      result = 'Rock is vaporized by Spock - You lose';
+    }
+
+    // SCISSORS RESULTS
+    else if (me === 'scissors' && computer === 'paper') {
+      result = 'Scissors cut paper - You win.';
+    }
+    else if (me === 'scissors' && computer === 'lizard') {
+      result = 'Scissors decapitated the lizard - You win';
+    }
+    else if (me === 'scissors' && computer === 'rock') {
+      result = 'Scissors are crushed by the rock - You lose';
+    }
+    else if (me === 'scissors' && computer === 'spock') {
+      result = 'Scissors are crushed by Spock - You lose';
+    }
+
+    // PAPER RESULTS
+    else if (me === 'paper' && computer === 'rock') {
+      result = 'Paper covers rock - You win.';
+    }
+    else if (me === 'paper' && computer === 'spock') {
+      result = 'Paper disproves Spock - You win';
+    }
+    else if (me === 'paper' && computer === 'scissors') {
+      result = 'Paper is cut by scissors - You lose';
+    }
+    else if (me === 'paper' && computer === 'lizard') {
+      result = 'Paper is eaten by lizard - You lose';
+    }
+
+    // SPOCK RESULTS
+    else if (me === 'spock' && computer === 'scissors') {
+      result = 'Spock smashes rock - You win.';
+    }
+    else if (me === 'spock' && computer === 'rock') {
+      result = 'Spock vaporizes - You win';
+    }
+    else if (me === 'spock' && computer === 'paper') {
+      result = 'Spock is disproved by paper - You lose';
+    }
+    else if (me === 'spock' && computer === 'lizard') {
+      result = 'Spock is poisoned by lizard - You lose';
+    }
+
+    // LIZARD RESULTS
+    else if (me === 'lizard' && computer === 'spock') {
+      result = 'Lizard poisons Spock - You win.';
+    }
+    else if (me === 'lizard' && computer === 'paper') {
+      result = 'Lizard eats paper - You win';
+    }
+    else if (me === 'lizard' && computer === 'rock') {
+      result = 'Spock is disproved by paper - You lose';
+    }
+    else if (me === 'lizard' && computer === 'scissors') {
+      result = 'Spock is poisoned by lizard - You lose';
+    }
+
+    console.log(result);
+  }
+}
+
+export default GameMechanics;
