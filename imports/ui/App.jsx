2f47d1488e61e555b14119c36559c9689a7a8d4b
David Espinoza <despinoza1027@gmail.com>
2017-07-09 21:07:06 -0500

    set up initial templating

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -34,24 +34,45 @@ export default class App extends Component {
   render() {
     return (
       <div className="container">
-        <p>Rock-Paper-Scissors-Lizard-Spock</p>
-        <select onChange={ (evt) => this.playerSelect(evt) }>
-          <option value="Rock">Rock</option>
-          <option value="Paper">Paper</option>
-          <option value="Scissors">Scissors</option>
-          <option value="Lizard">Lizard</option>
-          <option value="Spock">Spock</option>
-        </select>
-        <button onClick={ () => this.playGame() }>Play</button>
+        <nav>
+          <p className="title">Rock-Paper-Scissors-Lizard-Spock</p>
+          <p className={this.state.resultVisibility} id="score">
+            Your Score: {this.state.playerScore} Computer's Score: {this.state.computerScore}
+          </p>
+        </nav>
+
+        <div>
+          <p>Choose Your Weapon</p>
+          <select onChange={ (evt) => this.playerSelect(evt) }>
+            <option value="Rock">Rock</option>
+            <option value="Paper">Paper</option>
+            <option value="Scissors">Scissors</option>
+            <option value="Lizard">Lizard</option>
+            <option value="Spock">Spock</option>
+          </select>
+          <button onClick={ () => this.playGame() }>Play</button>
+        </div>
+
         <div className={this.state.resultVisibility}>
-          <p>Your Score: {this.state.playerScore}</p>
-          <p>Computer's Score: {this.state.computerScore}</p>
-          <p>Your Pick: {this.state.playerDecision}</p>
-          <img src={this.state.playerDecisionImg} alt="player choice"/>
-          <p>VS.</p>
-          <p>Computer's Pick: {this.state.computerDecision}</p>
-          <img src={this.state.computerDecisionImg} alt="computer choice"/>
-          <p>{this.state.result}</p>
+
+          <div className="selection-zone">
+            <div className="selection player-selection">
+              <p>Your Pick: {this.state.playerDecision}</p>
+              <img src={this.state.playerDecisionImg} alt="player choice"/>
+            </div>
+
+            <div className="selection versus">
+              <img src="images/versus.jpg" alt="versus"/>
+            </div>
+
+            <div className="selection computer-selection">
+              <p>Computer's Pick: {this.state.computerDecision}</p>
+              <img src={this.state.computerDecisionImg} alt="computer choice"/>
+            </div>
+          </div>
+
+
+          <p className="result">{this.state.result}</p>
         </div>
       </div>
     );
